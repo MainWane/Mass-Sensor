6334a834562be5325b11897cecb19cf71e61b7d9
MainWane <ulrikwandall@outlook.com>
2025-05-26 20:16:05 +0200

    funktionel knap kode

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,21 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#define BUTTON_PIN 4 // GIOP33 pin connected to button
-#define LED_PIN 17 // GIOP21 pin connected to LED
+// I2C-adresse: oftest 0x27 eller 0x3F
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  // initialize the pushbutton pin as an pull-up input
-  // the pull-up input pin will be HIGH when the switch is open and LOW when the switch is closed.
-  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
-  pinMode(LED_PIN, OUTPUT);
+  Wire.begin();  // Starter I2C-bussen
+  lcd.init();   // Initialiser LCD
+  lcd.backlight(); // Tænd baggrundslys
+
+  lcd.setCursor(0, 0);
+  lcd.print("Hej fra ESP32!");
+  lcd.setCursor(0, 1);
+  lcd.print("Laus er en skumfidus :)");
 }
 
 void loop() {
-  // read the state of the switch/button:
-  int buttonState = digitalRead(BUTTON_PIN);
-
-  // print out the button's state
-  Serial.println(buttonState);
-
-  // if the button is pressed (LOW), turn on the LED
-  if (buttonState == LOW) {
-    digitalWrite(LED_PIN, LOW);
-  } else {
-    digitalWrite(LED_PIN, HIGH);
-  }
+  // LCD viser tekst konstant - intet i loop
 }
